2e7ee3fcde28f1d3dfe110a40d423c3e4b3babc9
talaatmagdyx <talaatmagdy75@gmail.com>
2023-06-21 23:53:11 +0300

    added documenation

--- lib/socials_regex.rb
+++ lib/socials_regex.rb
@@ -2,8 +2,8 @@
 
 require_relative 'socials_regex/version'
 
+#
 module SocialsRegex
-  autoload :Error, 'socials_regex/error'
   autoload :Platforms, 'socials_regex/platforms'
   autoload :Regexes, 'socials_regex/platforms'
 end
